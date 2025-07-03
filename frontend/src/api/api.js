518531325456622108bad6c21a73f620db0c6868
saikiran <myadari.saikiran45@gmail.com>
2025-07-03 16:37:46 +0530

    resolved

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://auction-platform-mern-stack-1.onrender.com",
+  baseURL: "https://auction-platform-mern-stack.onrender.com/api/v1",
   withCredentials: true,
 });
 
