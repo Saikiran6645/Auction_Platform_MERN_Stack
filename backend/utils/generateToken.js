d0157a86f694dd44ba4a3b0c12618b5f961abd3f
saikiran <myadari.saikiran45@gmail.com>
2025-07-03 17:54:10 +0530

    error resolved

--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -1,7 +1,6 @@
-export const generateToken = (user, statusCode,  message,res) => {
+export const generateToken = (user, statusCode, message, res) => {
   const token = user.getJwtToken();
 
-  
   res
     .status(statusCode)
     .cookie("token", token, {
@@ -9,6 +8,8 @@ export const generateToken = (user, statusCode,  message,res) => {
         Date.now() + process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000
       ),
       httpOnly: true,
+      sameSite: "None", // For cross-origin requests
+      secure: true,
     })
     .json({
       success: true,
--- frontend/src/layouts/SideDrawer.jsx
+++ frontend/src/layouts/SideDrawer.jsx
@@ -15,7 +15,7 @@ const SideDrawer = () => {
   const [show, setShow] = useState(false);
   const dispatch = useDispatch();
   const { isAuthenticated, user, role } = useSelector((state) => state.user);
-
+  console.log(user, role);
   const handleLogout = () => {
     dispatch(logout());
   };
